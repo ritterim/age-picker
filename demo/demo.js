3a26aebcc77de7c0cddf82b28cca38de8256371d
Ken Dale <ken@kendaleiv.com>
2016-10-17 17:19:42 -0400

    Add Custom Events Log to demo page

--- demo/demo.js
+++ demo/demo.js
@@ -2,18 +2,23 @@ new AgePicker({
   selectClasses: ['form-control']
 }).init();
 
+var eventsLog = document.getElementById('events-log');
+
 var ids = [
   {
+    elementId: 'age',
     ageValue: 'age-value',
     result: 'result'
   },
   {
+    elementId: 'direct-entry-only-age',
     ageValue: 'direct-entry-only-age-value',
     result: 'direct-entry-only-result'
   }
 ];
 
 ids.forEach(idSet => {
+  var element = document.getElementById(idSet.elementId);
   var ageValue = document.getElementById(idSet.ageValue);
   var result = document.getElementById(idSet.result);
 
@@ -21,5 +26,12 @@ ids.forEach(idSet => {
 
   ageValue.addEventListener('change', e => {
     result.innerHTML = e.target.value;
+  });
+
+  element.addEventListener('ageChanged', e => {
+    var li = document.createElement('li');
+    li.innerText = e.target.id + ', ' + e.type + ', value: ' + e.value;
+
+    eventsLog.appendChild(li);
   });
 });
